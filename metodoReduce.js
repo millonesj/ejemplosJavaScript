fcbb3bcb5953ebf112a5fd796c0983795e934a8d
Jhonny Neciosup <jhonny.millones@gmail.com>
2019-08-05 22:53:49 -0500

    ejemplo usando el metodo reduce

--- metodoReduce.js
+++ metodoReduce.js
@@ -1,29 +1,32 @@
+/* Ejemplo de uso del método reduce.
+Tenemos un array de objetos tipo carro, usamos reduce para obtener un nuevo array con solo los objetos cuyo atributo es: a
+ctivo = true */
 const carros = [
-	  {
-		      marca: "Honda",
-		      activo: true
-		    },
-	  {
-		      marca: "Hyundai",
-		      activo: true
-		    },
-	  {
-		      marca: "Toyota",
-		      activo: true
-		    },
-	  {
-		      marca: "Mazda",
-		      activo: false
-		    }
+	{
+		marca: "Honda",
+		activo: true
+	},
+	{
+		marca: "Hyundai",
+		activo: true
+	},
+	{
+		marca: "Toyota",
+		activo: true
+	},
+	{
+		marca: "Mazda",
+		activo: false
+	}
 ];
 
 const reducido = carros.reduce(function(nuevaLista, objetoItera) {
-	  if (objetoItera.activo) {
-		      nuevaLista.push(objetoItera);
-		      return nuevaLista;
-		    } else {
-			        return nuevaLista;
-			      }
+	if (objetoItera.activo) {
+		nuevaLista.push(objetoItera);
+		return nuevaLista;
+	} else {
+		return nuevaLista;
+	}
 }, []);
 
 console.log(reducido);
