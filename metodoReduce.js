031a8d74873a6c5db7af87a9f5cf051d6cda5bbd
Jhonny Neciosup <jhonny.millones@gmail.com>
2019-08-07 17:33:19 -0500

    agrega el ejemplo de quitar duplicados

--- metodoReduce.js
+++ metodoReduce.js
@@ -30,3 +30,17 @@ const reducido = carros.reduce(function(nuevaLista, objetoItera) {
 }, []);
 
 console.log(reducido);
+
+/*
+::: Quitar duplicados de Array :::
+ */
+var lista = [1, 2, 3, 4, 5, 6, 1, 1, 2, 3, 4, 5, 8, 9, 1, 2, 5, 4, 4, 7, 9];
+
+const sinRepetidos = lista.reduce(function(nueva, row) {
+  if (nueva.indexOf(row) < 0) {
+		nueva.push(row)
+	};
+  return nueva;
+}, []);
+
+console.log(sinRepetidos);
