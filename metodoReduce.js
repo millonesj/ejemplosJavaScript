2216f2ef592a2c48cbe6aee9a4a8a97fa5d56549
Jhonny Neciosup <jhonny.millones@gmail.com>
2019-08-08 13:03:19 -0500

    feat(metodoReduce.js): agrega ejemplo de agrupar objetos en array

--- metodoReduce.js
+++ metodoReduce.js
@@ -30,7 +30,12 @@ const reducido = carros.reduce(function(nuevaLista, objetoItera) {
 }, []);
 
 console.log(reducido);
+/* [ { marca: 'Honda', activo: true },
+  { marca: 'Hyundai', activo: true },
+  { marca: 'Toyota', activo: true } ] */
 
+
+console.log("= * =");
 /*
 ::: Quitar duplicados de Array :::
  */
@@ -43,4 +48,73 @@ const sinRepetidos = lista.reduce(function(nueva, row) {
   return nueva;
 }, []);
 
-console.log(sinRepetidos);
+console.log(sinRepetidos); // [ 1, 2, 3, 4, 5, 6, 8, 9, 7 ]
+
+console.log("= * =");
+/* 
+	::: Retorna un nuevo array con los productos agrupados segun categoria 
+ */
+var listaDeProductos = [
+  {
+    categoria: "aceites",
+    nombre: "Oliva"
+  },
+  {
+    categoria: "arroz",
+    nombre: "extra"
+  },
+  {
+    categoria: "aceites",
+    nombre: "vegetal"
+  },
+  {
+    categoria: "arroz",
+    nombre: "superior"
+  },
+  {
+    categoria: "arroz",
+    nombre: "integral"
+  },
+  {
+    categoria: "aceites",
+    nombre: "especial"
+  }
+];
+
+const agrupadosPorCategoria = listaDeProductos.reduce(function(
+  productosPorCategoria,
+  producto,
+  indiceActual
+) {
+  if (indiceActual === 0) {
+    let nuevaCategoria = {
+      categoria: producto.categoria,
+      productos: [producto.nombre]
+    };
+    productosPorCategoria.push(nuevaCategoria);
+  } else {
+    let existe = false;
+    productosPorCategoria.forEach((element, indice) => {
+      if (element.categoria === producto.categoria) {
+        productosPorCategoria[indice].productos.push(producto.nombre);
+        existe = true;
+      }
+    });
+    if (!existe) {
+      let nuevaCategoria = {
+        categoria: producto.categoria,
+        productos: [producto.nombre]
+      };
+      productosPorCategoria.push(nuevaCategoria);
+    }
+  }
+
+  return productosPorCategoria;
+},
+[]);
+
+console.log(agrupadosPorCategoria);
+/* [ { categoria: 'aceites',
+    productos: [ 'Oliva', 'vegetal', 'especial' ] },
+  { categoria: 'arroz',
+    productos: [ 'extra', 'superior', 'integral' ] } ] */
